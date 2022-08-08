6abed5dd374d3995af761934e211c3c6630be6a9
unknown <you@example.com>
2022-08-08 17:05:25 +0300

    search and delete employee

--- Exam/EmployeesApp/EmployeesApp/Areas/Employee/Controllers/EmployeeController.cs
+++ Exam/EmployeesApp/EmployeesApp/Areas/Employee/Controllers/EmployeeController.cs
@@ -2,6 +2,7 @@
 using EmployeesApp.ApplicationContext;
 using EmployeesApp.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
 namespace EmployeesApp.Areas.Employees.Controllers
 {
@@ -21,6 +22,23 @@ namespace EmployeesApp.Areas.Employees.Controllers
             return View(await db.Employees.ToListAsync());
         }
 
+        [HttpPost]
+        public async Task<IActionResult> EmployeeFilteredList(string? searchString)
+        {
+            string patternDate = "^([0]?[0-9]|[12][0-9]|[3][01])[./-]([0]?[1-9]|[1][0-2])[./-]([0-9]{4}|[0-9]{2})$";
+            var employees = from m in db.Employees
+                         select m;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                employees = employees.Where(employee =>
+                    employee.Name.Contains(searchString) ||
+                    employee.Age.ToString().Contains(searchString) ||
+                    employee.Speciality.Contains(searchString) ||
+                    (Regex.IsMatch(searchString, patternDate) && employee.EmployementDate == DateTime.Parse(searchString)));
+            }
+            return View("EmployeeList", await employees.ToListAsync());
+        }
+
         [Route("Create")]
         public IActionResult Create()
         {
@@ -30,6 +48,7 @@ namespace EmployeesApp.Areas.Employees.Controllers
         [HttpPost("Create")]
         public async Task<IActionResult> Create(Employee employee)
         {
+            //------------TODO ERROR-------------
             await db.Employees.AddAsync(employee);
             await db.SaveChangesAsync();
             return RedirectToAction("EmployeeList");
@@ -44,7 +63,7 @@ namespace EmployeesApp.Areas.Employees.Controllers
             {
                 Employee? employee = await db.Employees.FirstOrDefaultAsync(x => x.Id == id);
                 if (employee != null) return View("CreateEditEmployee", employee);
-                //------------TODO-------------
+                //------------TODO ERROR-------------
                 else return NotFound();
             }
             return View();
@@ -54,9 +73,24 @@ namespace EmployeesApp.Areas.Employees.Controllers
         [HttpPost("Edit")]
         public async Task<IActionResult> Edit(Employee employee)
         {
+            //------------TODO ERROR-------------
             db.Employees.Update(employee);
             await db.SaveChangesAsync();
             return RedirectToAction("EmployeeList");
+        }
+
+        [HttpPost("Delete/{id}")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                Employee user = new Employee { Id = id.Value };
+                db.Entry(user).State = EntityState.Deleted;
+                await db.SaveChangesAsync();
+                return RedirectToAction("EmployeeList");
+            }
+            //------------TODO ERROR-------------
+            return NotFound();
         }
     }
 }
